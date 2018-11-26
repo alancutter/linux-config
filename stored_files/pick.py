32936674ba7de5ad0d28d7a166d4ab610ab9efd2
Alan Cutter <alancutter@chromium.org>
2018-11-26 17:06:57 +1100

    Update

--- stored_files/pick.py
+++ stored_files/pick.py
@@ -2,6 +2,19 @@ from __future__ import print_function
 import sys
 
 
+def pick_bool(prompt):
+  while True:
+    try:
+      input = raw_input(prompt + ' ([y]/n): ')
+      if input in ('', 'y'):
+        return True
+      elif input == 'n':
+        return False
+    except KeyboardInterrupt:
+      raise
+    except:
+      print('Invalid input')
+
 def pick_item(items, prompt, file=sys.stdout):
   while True:
     for item in enumerate(items):
